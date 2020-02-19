40a65db2f77e5360886cff069a4748c26f5785f0
samanthayee18 <samanthayee@csu.fullerton.edu>
2020-02-19 13:59:04 -0800

    Add files via upload
    committed Ottergram

---
+++ OTTERGRAM/scripts/main.js
@@ -0,0 +1,79 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]'; 
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27; 
+
+function setDetails(imageURL, titleText){
+    'use strict';
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);  
+    detailImage.setAttribute('src', imageURL); 
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);  
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {  
+    'use strict';  
+    return thumbnail.getAttribute('data-image-url'); 
+}
+
+function titleFromThumb(thumbnail) {  
+    'use strict';  
+    return thumbnail.getAttribute('data-image-title'); 
+}
+
+function setDetailsFromThumb(thumbnail) {  
+    'use strict';  
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail)); 
+}
+
+function addThumbClickHandler(thumb) {  
+    'use strict'; 
+
+     thumb.addEventListener ('click', function (event) {  
+        event.preventDefault();    
+        setDetailsFromThumb(thumb) 
+        showDetails();
+    });
+}
+
+function getThumbnailsArray() {  
+    'use strict'; 
+     var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+     var thumbnailArray = [].slice.call(thumbnails);  
+     return thumbnailArray; 
+ } 
+
+ function hideDetails() {  
+    'use strict';  
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+ } 
+
+ function showDetails() {  
+     'use strict';  
+     document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+     } 
+
+ function addKeyPressHandler() {  
+    'use strict';  
+    document.body.addEventListener('keyup', function (event){
+    event.preventDefault();   
+    console.log(event.keyCode); 
+    if (event.keyCode === ESC_KEY) {
+        hideDetails(); 
+       } 
+        }); 
+    } 
+
+ function initializeEvents() { 
+      'use strict';  
+      var thumbnails = getThumbnailsArray(); 
+      thumbnails.forEach(addThumbClickHandler); 
+      addKeyPressHandler(); 
+    }
+
+    initializeEvents();
+
+
+
